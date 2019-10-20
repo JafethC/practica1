66c99209a2a14ebe10a941f2d8210ecae03fb9fa
JafethC <56799361+JafethC@users.noreply.github.com>
2019-10-20 12:37:58 -0500

    Add files via upload

---
+++ Practica1/ProyectoMPLAB5.25/Practica1.X/build/default/debug/Practica1.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Practica1.o:  \
+	C:/Users/Aftak/MPLABXProjects/Practica1.X/Practica1.asm  \
+
---
+++ Practica1/ProyectoMPLAB5.25/Practica1.X/build/default/production/Practica1.o.d
@@ -0,0 +1,3 @@
+build/default/production/Practica1.o:  \
+	C:/Users/Aftak/MPLABXProjects/Practica1.X/Practica1.asm  \
+
